12381ac573d0a81b85c3517fe7845e9f72fd76c3
Adarsh vishwakarma <vishwakarma1917adarsh@gmail.com>
2026-07-20 00:31:09 +0530

    update app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Browserrouter, Routes, Route, Navigate, useNavigate, useLocation, HashRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate, useNavigate, useLocation, HashRouter } from 'react-router-dom';
 import {
   Users, Calendar, CheckSquare, TrendingUp, LogOut, Lock, User, Plus,
   Search, Edit2, ShieldAlert, Check, X, Clock, AlertCircle, RefreshCw, ChevronRight, Activity, CircleAlert
@@ -1578,9 +1578,9 @@ const MainApp = () => {
 
 const App = () => {
   return (
-    <Browserrouter>
+    <BrowserRouter>
       <MainApp />
-    </Browserrouter>
+    </BrowserRouter>
   );
 };
 
